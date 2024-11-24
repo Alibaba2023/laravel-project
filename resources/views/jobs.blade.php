a6c1a01049c21c6b02494419fcd1cfb1c51f8a75
Ali Baba <ali7090370@gmail.com>
2024-11-24 20:11:38 +0430

    style added to jobs list

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,7 +5,7 @@
     <ul>
 
         @foreach ($jobs as $job)
-            <li class="bg-green-200 p-2 m-2 rounded-lg hover:bg-green-300 hover:underline">
+            <li class="bg-green-200 p-2 m-2 rounded-lg hover:bg-green-300">
                 <a href="/jobs/{{ $job['id'] }}">
                     The tital of the job: {{ $job['title'] }}.The salary of the job: {{ $job['salary'] }}
                 </a>
